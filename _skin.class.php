e9ea4a5b75bf701791be616eb9511a659eb66961
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-09-06 16:11:36 +0200

    skin version update (0.1 for pre-released skins)

--- _skin.class.php
+++ _skin.class.php
@@ -21,7 +21,7 @@ class miniblog_Skin extends Skin
 	 * Skin version
 	 * @var string
 	 */
-	var $version = '1.0.0';
+	var $version = '0.1';
 
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
